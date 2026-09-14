5bb777664b356ced03a86517b30b2e923578994c
Logan <95306463+LaganYT@users.noreply.github.com>
2026-09-13 22:33:41 -0500

    feat: sandbox embedded playback

--- apps/web/components/MediaDetailShell.tsx
+++ apps/web/components/MediaDetailShell.tsx
@@ -3,15 +3,24 @@ type MediaDetailShellProps = {
   title?: string;
 };
 
+const PLAYER_SANDBOX = [
+  "allow-scripts",
+  "allow-same-origin",
+  "allow-forms",
+  "allow-presentation",
+].join(" ");
+
 export default function MediaDetailShell({ embedUrl, title }: MediaDetailShellProps) {
   return (
     <div className="watch-screen">
       <iframe
         src={embedUrl}
         title={title}
-        allowFullScreen
-        allow="autoplay; fullscreen; picture-in-picture"
         className="watch-frame"
+        sandbox={PLAYER_SANDBOX}
+        referrerPolicy="no-referrer"
+        allow="autoplay; fullscreen; picture-in-picture; encrypted-media"
+        allowFullScreen
       />
     </div>
   );
